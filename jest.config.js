d13bb676be8cc4868da60dd1145d966aa5f3c559
Kiram Rahim <kiramrahim@gmail.com>
2022-09-04 21:06:00 +0100

    now generating code coverage report

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,6 @@ module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   globalSetup: 'jest-preset-angular/global-setup',
+  collectCoverage: true,
+  coverageReporters: ["json", "html"]
 };
